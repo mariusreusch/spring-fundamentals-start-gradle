e37eff0c9884ea573459a17444136b445356203e
Marius Reusch <marius.reusch1@swisscom.com>
2022-09-03 17:13:02 +0200

    intial commit

---
+++ src/main/java/com/springfundamentals/SpringFundamentalsStartGradleApplication.java
@@ -0,0 +1,13 @@
+package com.springfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalsStartGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFundamentalsStartGradleApplication.class, args);
+    }
+
+}
